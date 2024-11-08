59932912687f925777c986ed3019e2fe6e0924a9
LeDok <59102221+ledokua@users.noreply.github.com>
2024-11-08 09:42:18 +0100

    Практична

---
+++ Залік 4 завдання/4.py
@@ -0,0 +1,8 @@
+def sort_words(words):
+    return sorted(words)
+
+# Приклад використання
+words_list = ["яблуко", "банан", "апельсин", "груша", "ананас"]
+sorted_list = sort_words(words_list)
+
+print("Відсортований список:", sorted_list)
